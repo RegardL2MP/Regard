f088e8888d4e0cf7bf9c63c9ac13e20470d70582
Theo Bourdon <theo.studies42@gmail.com>
2018-02-21 14:10:35 +0100

    squelette preprocessor

--- zPreprocessor.py
+++ zPreprocessor.py
@@ -2,15 +2,28 @@
 """
 Ceci est reserve au preprocesseurs
 """
+from sklearn.base import BaseEstimator 
 
-from sklearn.base import BaseEstimator
+def identity(x): 
 
-def idd(x):
-    return x
+    return x 
 
-class PreProcess(BaseEstimator):
-    def __init__(self, transform=idd):
-        self.transform = idd
-        
-    #rajouter les methodes pour nettoyer les donnees
-        
+class SimpleTransform(BaseEstimator): 
+
+    def __init__(self, transformer=identity): 
+
+        self.transformer = transformer 
+
+    def fit(self, X, y=None): 
+
+        return self 
+
+    def fit_transform(self, X, y=None): 
+
+        return self.transform(X) 
+
+    def transform(self, X, y=None): 
+
+        return np.array([self.transformer(x) for x 
+
+in X], ndmin=2).T 
