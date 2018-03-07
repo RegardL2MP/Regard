d4186d4403d3233ca71504eca71f2c2d22937b9b
Theo Bourdon <theo.studies42@gmail.com>
2018-03-07 14:02:38 +0100

    Changements Preprocessor, rajout de code

--- zPreprocessor.py
+++ zPreprocessor.py
@@ -3,6 +3,7 @@
 Ceci est reserve au preprocesseurs
 """
 from sklearn.base import BaseEstimator 
+import loadData.py
 
 def identity(x): 
 
@@ -27,3 +28,28 @@ class SimpleTransform(BaseEstimator):
         return np.array([self.transformer(x) for x 
 
 in X], ndmin=2).T 
+
+# Pour nettoyer le dataset des entrées incomplètes
+def nettoyer_bdd(dataset):
+    #on fait une copie de la liste pour éviter de modifier la liste de base
+    cleaned_list = list(dataset)
+    for i in range(len(cleaned_list)):
+        #si il y a moins de 256 features alors le dataset est incomplète
+        if(len(cleaned_list[i]) != 256):
+            #donc on retire de notre liste l'entrée incriminée
+            cleaned_list.pop(i)
+    #puis on renvoie la liste nettoyée
+    return cleaned_list
+
+# Permet de normaliser les valeurs du dataset
+# mini/maxi = valeur minimale/maximale qu'on souhaite obtenir pour chaque valeur
+def normaliser_bdd(dataset, mini, maxi):
+    if(mini == maxi or maxi < mini):
+        raise ValueError("valeurs mini maxi fausses")
+    normalized_list = list(dataset)    
+    #pour chaque entrée
+    for i in range(len(normalized_list)):
+        for j in range(len(normalized_list[i])):
+            #formule de la normalisation
+            normalized_list[i][j] = (normalized_list[i][j] - mini) / (maxi - mini)
+    return normalized_list
